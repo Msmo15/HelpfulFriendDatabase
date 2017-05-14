d6a416541dd615efd22c572c2b5a1cbbc5b28c70
Msmo15 <exxri@hotmail.com>
2017-05-14 22:19:02 +0200

    awd

---
+++ HelpfulFriendDatabase/Job.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Job]
+(
+	[JobID] INT NOT NULL,
+	[JobTitle] VARCHAR(50) NOT NULL,
+	[Category] VARCHAR(250) NOT NULL,
+	[Address] VARCHAR(50) NOT NULL,
+	[JobDate] DATETIME NULL,
+	[Price] INT NULL,
+	PRIMARY KEY CLUSTERED ([JobID] ASC),
+	CONSTRAINT [FK_dbo.Job_dbo.Category_Category] FOREIGN KEY ([Category])
+		REFERENCES [dbo].[Category]([Category])
+)
